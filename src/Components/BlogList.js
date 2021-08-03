e81e5a6de1ff5480b12271711281a07236971508
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 03:06:08 -0700

    Reusing a component and destructuring props

---
+++ src/Components/BlogList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function BlogList({ blogs, title }) {
+
+  return (
+    <div className='blog__list'>
+      <p>{title}</p>
+      {blogs.map((blog) => (
+        <div className="blog__preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BlogList
