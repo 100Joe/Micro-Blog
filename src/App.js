3356f6834e14c0a02d1f8d9920e96d163002d8a5
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 01:50:44 -0700

    Nested components and importing components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
-  // Numbers, strings, and Arrays are fined However Booleans and Objects cannot be output for dynamic values. 
-  const title = 'Welcome to JavaScript and Friends';
-  const likes = 50;
+
 
   return (
     <div className="App">
-      {/* Dynamic values can be used in {} */}
-      <h1>{title}</h1>
-      <p>Liked {likes} times</p>
+      <Navbar />
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
