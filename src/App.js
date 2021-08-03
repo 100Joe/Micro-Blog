5d990baad2788c3ccd741a3f6957df33755fc171
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 01:42:54 -0700

    Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  // Numbers, strings, and Arrays are fined However Booleans and Objects cannot be output for dynamic values. 
+  const title = 'Welcome to JavaScript and Friends';
+  const likes = 50;
+
   return (
     <div className="App">
-      <h1>Hello EveryOne!</h1>
+      {/* Dynamic values can be used in {} */}
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
     </div>
   );
 }
