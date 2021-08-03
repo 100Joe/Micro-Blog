40e643467bd86a9a056c3323f87fd109ab357548
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 02:30:23 -0700

    Creating and using Hooks. useState()

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,20 @@
-import React from 'react';
-
-const handleClick = (e) => {
-  console.log(` Hello `, e)
-}
-
-const handleClickAgain = (name, e) => {
-  console.log(` Hello ${name}`, e.target)
-}
+import React, { useState } from 'react';
 
 function Home() {
+  // let name = 'Joseph'; Not a reactive value
+  const [name, setName] = useState('Joseph');
+  const [age, setAge] = useState(27);
+
+  const handleClick = () => {
+    setName('Jordan');
+    setAge(28)
+  }
+
   return (
     <div className="home">
-      <h1>Welcome Home!</h1>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) =>
-        handleClickAgain('Joseph', e)
-      }>Click me again</button>
+      <h2>Welcome to Home {name} is {age} years old</h2>
+      <button onClick={handleClick}>Click here</button>
     </div>
-  )
+  );
 }
-
-export default Home
+export default Home;
