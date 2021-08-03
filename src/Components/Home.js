b48960f538e26facf9078ac11e2fdd656fa43f61
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 02:20:17 -0700

    Click events and event properties

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const handleClick = (e) => {
+  console.log(` Hello `, e)
+}
+
+const handleClickAgain = (name, e) => {
+  console.log(` Hello ${name}`, e.target)
+}
+
 function Home() {
   return (
     <div className="home">
       <h1>Welcome Home!</h1>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) =>
+        handleClickAgain('Joseph', e)
+      }>Click me again</button>
     </div>
   )
 }
