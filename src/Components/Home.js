81788beda66246be5aae68676448a9f379975acd
Joseph Edmonds <consultingjde@gmail.com>
2021-08-03 17:46:20 -0700

    useEffect and it's dependencies

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 function Home() {
@@ -7,16 +7,21 @@ function Home() {
     { title: "New breakthrough", body: 'lorem ipsum....', author: 'Jose', id: 2 },
     { title: "New tale", body: 'lorem ipsum....', author: 'Joe', id: 3 }
   ]);
+  const [name, setName] = useState('Joseph')
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log(name)
+  }, [name]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs' />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Joseph')} title="Joseph's blogs" handleDelete={handleDelete} />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Joe')}>Change Name</button>
     </div>
   );
 }
