2a6e49d40e957f82b1e7f63db1c37e21c6d7ffae
Joseph Edmonds <consultingjde@gmail.com>
2021-08-04 00:01:12 -0700

    Exact match routes and create component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Components/Create';
 
 function App() {
 
@@ -12,8 +13,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
